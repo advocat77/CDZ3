dc6e46541ffa54c8c10d1f9b8417e0f3f3e7cf30
Alex <advocat6787428@gmail.com>
2023-07-17 21:13:20 +0200

    second commit

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿void ShowQuarter(int ax, int ay, int bx, int by, int az, int bz)
+{
+   char[] array = input.ToCharArray();
+   Console.WriteLine($"Число: {distance}");
+}
+int GetInput(string text)
+{
+   Console.WriteLine(text);
+   return (Convert.ToInt32(Console.ReadLine()));
+}
+int ax = GetInput("Введите координату ax: ");
+int ay = GetInput("Введите координату ay: ");
+int bx = GetInput("Введите координату bx: ");
+int by = GetInput("Введите координату by: ");
+int az = GetInput("Введите координату az: ");
+int bz = GetInput("Введите координату bz: ");
