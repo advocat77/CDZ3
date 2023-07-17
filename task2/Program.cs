409c643c61e55db2894e3fc4ff2ddeb84c2022a0
Alex <advocat6787428@gmail.com>
2023-07-17 20:59:17 +0200

    task2 added

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿void ShowQuarter(int ax, int ay, int bx, int by, int az, int bz)
+{
+   double distance = Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2));
+   Console.WriteLine($"Расстояние: {distance}");
+}
+int GetInput(string text)
+{
+   Console.WriteLine(text);
+   return (Convert.ToInt32(Console.ReadLine()));
+}
+int ax = GetInput("Введите координату ax: ");
+int ay = GetInput("Введите координату ay: ");
+int bx = GetInput("Введите координату bx: ");
+int by = GetInput("Введите координату by: ");
+int az = GetInput("Введите координату az: ");
+int bz = GetInput("Введите координату bz: ");
+ShowQuarter(ax, ay, bx, by, az, bz);
